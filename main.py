d7dafc8aa13069f8a195ead97dc8a6a568c98d43
Remi Chen <yipin987@gmail.com>
2023-06-28 21:27:46 +0800

    debug forget to add main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import about
 
 ## I'm using streamlit to create my bolg, I want web browser title to set "Dan's record blog" and page title as "Welcome to Dan's space"
 
@@ -8,10 +9,12 @@ st.title('Welcome to Dan\'s space')
 
 st.sidebar.title('Navigation')
 #Good, now side bar should contain radio selector with some category ['Read books','Online course',]
-selector = st.sidebar.radio('my category',['Read books','Online course',])
+selector = st.sidebar.radio('my category',['About me','Read books','Online course',])
 if selector == 'Read books':
     st.write('You selected Read books')
 elif selector == 'Online course':
     st.write('You selected Online course')
+elif selector == 'About me':
+    about.about()
 
 
