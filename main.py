5af2f83c11bb293c561cb3cbd8113cab91404d60
Remi Chen <yipin987@gmail.com>
2023-06-28 21:15:10 +0800

    add side bar

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.write('Hello World')
+## I'm using streamlit to create my bolg, I want web browser title to set "Dan's record blog" and page title as "Welcome to Dan's space"
+
+st.set_page_config(page_title="Dan's record blog", page_icon=":smile:", layout="wide")
+st.title('Welcome to Dan\'s space')
+#Good, now I want to enable a side bar to control the content of my blog
+
+st.sidebar.title('Navigation')
+#Good, now side bar should contain radio selector with some category ['Read books','Online course',]
+selector = st.sidebar.radio('my category',['Read books','Online course',])
+if selector == 'Read books':
+    st.write('You selected Read books')
+elif selector == 'Online course':
+    st.write('You selected Online course')
+
+
