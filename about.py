9b1b9d09ccd62ef2e5905ef9090e8e166cbe6974
1daniel3333 <p123456dan@gmail.com>
2024-02-17 10:07:19 +0800

    change structure

--- about.py
+++ about.py
@@ -17,7 +17,13 @@ def about():
              I learn these knowleade from online course, books and from my own research.
              Please check "online course" for more information of my complete courses.
              """)
-    
+
+def gen_list_for_markdoen_hyperlink(input_dict:dict)->list:
+    res = []
+    for key, value in input_dict:
+        res.append(f'&#x2022; <a href="{value}">{key}</a><br>')
+    return res
+
 def learning():
     """
     connect to main
@@ -25,25 +31,32 @@ def learning():
     """
     st.title("My learning")
     st.markdown("<h3>Online course</h3>", unsafe_allow_html=True)
-    st.markdown("""<p>
-                &#x2022; <a href="https://www.coursera.org/learn/project-management-foundations/">Foundations of Project Management</a><br>
-                &#x2022; <a href="https://www.coursera.org/learn/generative-ai-for-everyone/">Generative AI for Everyone</a><br>
-                &#x2022; <a href="https://www.coursera.org/learn/ai-for-everyone">Coursera- AI for Everyone</a><br>
-                &#x2022; <a href="https://www.coursera.org/professional-certificates/google-data-analytics">Coursera- Google Data Analysis</a><br>
-                &#x2022; <a href="https://www.coursera.org/learn/python">Coursera - Programming for Everybody (Getting Started with Python)</a><br>
-                &#x2022; <a href="https://www.coursera.org/learn/learning-how-to-learn">Coursera - Learning How to Learn: Powerful mental tools to help you master tough subjects</a><br>
-                &#x2022; <a href="https://www.coursera.org/projects/machine-learning-streamlit-python">Coursera - Build a Machine Learning Web App with Streamlit and Python</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/the-complete-sql-bootcamp/">Udemy- SQL Bootcamp 2022: Go from Zero to Hero</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/tableau10/">Udemy- Tableau 2020 A-Z:Hands-on Tableau Training for data science</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/tableau10-advanced/">Udemy- Tableau 20 Advanced Training Master Tableau in Data Science</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/complete-uipath-rpa-developer-course/">Udemy- Complete Uiath RPA Developer Course: Build 7 Robots</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/jmp-2020-hands-on-jmp-training-for-statistics/">Udemy- JMP Training for Statistics % Data Visualization</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/codegym-python/">Udemy- Python 基礎課程和網路爬蟲入門實戰</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/best-unix-linux-training-for-software-qa-testers/">Udemy- Unix, Linux training for beginners</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/project-management-improve-project-manager-skills/">Udemy- Project Management: The Closing Phase</a><br>
-                &#x2022; <a href="https://www.udemy.com/course/r-programming/">Udemy- R Programming A-Z™: R For Data Science With Real exercises</a><br>
-                &#x2022; <a href="https://hiskio.com/courses/1857/about">HiSKIO：打滾也要有技巧，Nick的專業成長實踐課</a><br>
-                </p>""", unsafe_allow_html=True)
+    course_dict = { 'Test-Driven Development Overview':'https://www.coursera.org/account/accomplishments/verify/UQQ2U6Z446JK?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course',
+                   'Clean Code':'https://www.udemy.com/certificate/UC-2056c4e3-9b17-44a4-ae72-775f2c997af9/',
+                   'SQL for Data Science':'https://www.coursera.org/account/accomplishments/verify/CUNRGMWTPR4L',
+                   'Foundations of Project Management':'https://www.coursera.org/account/accomplishments/verify/PF3E6YLJ3YMZ',
+                   'Generative AI for Everyone':'https://www.coursera.org/account/accomplishments/verify/UXVXGEQUH7ZY',
+                   'Crash Course on Python':'https://www.coursera.org/account/accomplishments/certificate/Q3ZNQFLGVW6K',
+                   'AI For Everyone':'https://www.coursera.org/account/accomplishments/certificate/LLGS24UD8YLC',
+                   'Build a Machine Learning Web App with Streamlit and Python':'https://www.coursera.org/account/accomplishments/verify/CD4AMNV9SLCQ',
+                   'Introduction to Github and Visual Studio Code': 'https://www.coursera.org/account/accomplishments/certificate/7MFWFFTDT5Y9',
+                   'Foundations of User Experience (UX) Design':'https://www.coursera.org/account/accomplishments/verify/H485WGFH7R8N',
+                   'Learning How to Learn: Powerful mental tools to help you master tough subjects':'https://www.coursera.org/account/accomplishments/verify/D2WBG249NB6X',
+                   'Running Distributed TensorFlow using Vertex AI':'https://www.coursera.org/account/accomplishments/verify/6YN7F2P5P9X3',
+                   'Google Data Analytics':'https://www.coursera.org/account/accomplishments/professional-cert/JSJFKSD3USK7',
+                   'Google Cloud Big Data and Machine Learning Fundamentals':'https://www.coursera.org/account/accomplishments/verify/QW6KL4ACBZQN',
+                   'How Google does Machine Learning':'https://www.coursera.org/account/accomplishments/verify/AJH5ENJNK9K5',
+                   'Programming for Everybody':'https://www.coursera.org/account/accomplishments/certificate/YZTCA49W2NCD',
+                   'R Programming A-Z™: R For Data Science With Real Exercises!':'https://www.udemy.com/certificate/UC-eb5c2145-296d-4658-9971-3bab3f6c861f/',
+                    'Python基礎課程和網路爬蟲入門實戰':'https://www.udemy.com/certificate/UC-f9b4f983-c8b7-40aa-a1e8-5310635228ba/',
+                    'The Complete SQL Bootcamp: Go from Zero to Hero':'https://www.udemy.com/certificate/UC-7d1118a0-91b8-4f0a-8875-68fdd18f2276/',
+                    'Tableau Advanced: Master Tableau in Data Science':'https://www.udemy.com/certificate/UC-50bb5517-0127-4254-9f39-b564729039b6/',
+                    'JMP Training for Statistics & Data Visualization':'https://www.udemy.com/certificate/UC-de141b41-2cc1-4450-b27f-86dce6776d7b/',
+                    'Tableau A-Z: Hands-On Tableau Training for Data Science':'https://www.udemy.com/certificate/UC-4e8acd1a-f2e2-44b2-a0d7-9791b312eb8a/',
+                    'HiSKIO：打滾也要有技巧，Nick的專業成長實踐課':'https://hiskio.com/courses/1857/about',
+    }
+    
+    st.markdown(f"""<p>{' '.join(gen_list_for_markdoen_hyperlink(course_dict))}</p>""", unsafe_allow_html=True)
     st.markdown("<h3>Books</h3>", unsafe_allow_html=True)
     st.markdown("""<p>
                 &#x2022; <a href="https://24h.pchome.com.tw/books/prod/DJAA2V-A90053G9M">Python錦囊妙計</a><br>
