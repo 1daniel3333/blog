835c8a8f1645c5b08e24c85b221a976a25964d13
1daniel3333 <p123456dan@gmail.com>
2024-03-13 21:53:01 +0800

    clean code

--- main.py
+++ main.py
@@ -40,84 +40,97 @@ def is_empty_option()->bool:
 def set_option_to_default(option_list:list):
     st.query_params['option'] = option_list[0]
 
+def get_action_read_book():
+    tags_key=tag_create()
+    #add key to display how many post have the same tag
+    post_dict={}
+    for key,values in tags_key.items():
+        post_dict[f'{key}: {len(values)} post(s)']=key
+    check_tags=st.sidebar.selectbox('Tags',['All']+list(post_dict.keys()))
+    #base on selection, use two dic conversion to filter articles
+    if check_tags == 'All':
+        check_blog=st.sidebar.selectbox('Blogs',blogs.book_dict.keys())
+    else:
+        check_blog=st.sidebar.selectbox('Blogs',list(tags_key[post_dict[check_tags]]))
+    st.title(check_blog)
+    read_books(check_blog)
+
+def get_action_my_atricles():
+    st.title('Some recent Article:')
+    with st.spinner('Retrieving data'):
+        blog_dic = medium.get_medium_dict()
+    for key,value in blog_dic.items():
+        st.markdown(f'<h3><a href="{value["url"]}">{key}</a></h3>', unsafe_allow_html=True)
+        st.markdown(value["content"])
+
+def get_action_online_course():
+    about.learning()
+
+def get_action_about_me():
+    about.about()
+
+def get_action_house_trend():
+    body = """
+            <div class='tableauPlaceholder' id='viz1691080442321' style='width: 800px; height: 1000px;'> 
+                <noscript>
+                    <a href='#'>
+                    <img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ho&#47;house_trend&#47;overall_trend&#47;1_rss.png' style='border: none' />
+                    </a>
+                </noscript>
+                <object class='tableauViz'  width='800' height='1000' style='display:none;'>
+                    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                    <param name='embed_code_version' value='3' /> 
+                    <param name='site_root' value='' />
+                    <param name='name' value='house_trend&#47;overall_trend' />
+                    <param name='tabs' value='no' />
+                    <param name='toolbar' value='yes' />
+                    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ho&#47;house_trend&#47;overall_trend&#47;1.png' /> 
+                    <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' />
+                    <param name='display_spinner' value='yes' />
+                    <param name='display_overlay' value='yes' />
+                    <param name='display_count' value='yes' />
+                    <param name='language' value='zh-TW' />
+                </object>
+            </div>                
+            <script type='text/javascript'>                    
+                var divElement = document.getElementById('viz1691080442321');                    
+                var vizElement = divElement.getElementsByTagName('object')[0];                    
+                vizElement.style.width='100%';
+                vizElement.style.height='100%';                   
+                var scriptElement = document.createElement('script');                    
+                scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+                vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+            </script>"""
+    html(body)
+    st.markdown('''<p><a href="https://public.tableau.com/views/house_trend/overall_trend?:language=zh-TW&:display_count=n&:origin=viz_share_link">
+                House Trending Tableau form</a></p>''', unsafe_allow_html=True)
+    st.info('python plot TBD.')
+
+def get_action_subscribe():
+    content_dict = {
+        '大人學列表': suscribe.get_big_man_rss(),
+        '綠角列表': suscribe.get_green_data()
+        }
+    with st.spinner('Retrieving data'):
+        for title, data in content_dict.items():
+            st.title(title)
+            for key, value in data.items():
+                body = f'<p><a href="{value}">{key}</a></p>'
+                st.markdown(body, unsafe_allow_html=True)
+        
 def decide_action_on_selection(selector:str):
     if selector == 'Read books':
-        tags_key=tag_create()
-        #add key to display how many post have the same tag
-        post_dict={}
-        for key,values in tags_key.items():
-            post_dict[f'{key}: {len(values)} post(s)']=key
-        check_tags=st.sidebar.selectbox('Tags',['All']+list(post_dict.keys()))
-        #base on selection, use two dic conversion to filter articles
-        if check_tags == 'All':
-            check_blog=st.sidebar.selectbox('Blogs',blogs.book_dict.keys())
-        else:
-            check_blog=st.sidebar.selectbox('Blogs',list(tags_key[post_dict[check_tags]]))
-        st.title(check_blog)
-        read_books(check_blog)
+        get_action_read_book()
     elif selector == 'my articles':
-        st.title('Some recent Article:')
-        with st.spinner('Retrieving data'):
-            blog_dic = medium.get_medium_dict()
-        for key,value in blog_dic.items():
-            st.markdown(f'<h3><a href="{value["url"]}">{key}</a></h3>', unsafe_allow_html=True)
-            st.markdown(value["content"])
-        
+        get_action_my_atricles()
     elif selector == 'Online course':
-        about.learning()
+        get_action_online_course()
     elif selector == 'About me':
-        about.about()
+        get_action_about_me()
     elif selector == 'house trend':
-        body = """
-                <div class='tableauPlaceholder' id='viz1691080442321' style='width: 800px; height: 1000px;'> 
-                    <noscript>
-                        <a href='#'>
-                        <img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ho&#47;house_trend&#47;overall_trend&#47;1_rss.png' style='border: none' />
-                        </a>
-                    </noscript>
-                    <object class='tableauViz'  width='800' height='1000' style='display:none;'>
-                        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
-                        <param name='embed_code_version' value='3' /> 
-                        <param name='site_root' value='' />
-                        <param name='name' value='house_trend&#47;overall_trend' />
-                        <param name='tabs' value='no' />
-                        <param name='toolbar' value='yes' />
-                        <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ho&#47;house_trend&#47;overall_trend&#47;1.png' /> 
-                        <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' />
-                        <param name='display_spinner' value='yes' />
-                        <param name='display_overlay' value='yes' />
-                        <param name='display_count' value='yes' />
-                        <param name='language' value='zh-TW' />
-                    </object>
-                </div>                
-                <script type='text/javascript'>                    
-                    var divElement = document.getElementById('viz1691080442321');                    
-                    var vizElement = divElement.getElementsByTagName('object')[0];                    
-                    vizElement.style.width='100%';
-                    vizElement.style.height='100%';                   
-                    var scriptElement = document.createElement('script');                    
-                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
-                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
-                </script>"""
-        html(body)
-        st.markdown('''<p><a href="https://public.tableau.com/views/house_trend/overall_trend?:language=zh-TW&:display_count=n&:origin=viz_share_link">
-                    House Trending Tableau form</a></p>''', unsafe_allow_html=True)
-        st.info('python plot TBD.')
+        get_action_house_trend()
     elif selector == 'subscribe':
-        st.title('大人學列表')
-        dict = suscribe.get_big_man_rss()
-        for key, value in dict.items():
-            body = f"""
-            <p><a href="{value}"</a>{key}</p>
-            """
-            st.markdown(body, unsafe_allow_html=True)
-        st.title('綠角列表')
-        dict = suscribe.get_green_data()
-        for key, value in dict.items():
-            body = f"""
-            <p><a href="{value}"</a>{key}</p>
-            """
-            st.markdown(body, unsafe_allow_html=True)
+        get_action_subscribe()
 
 def show_sidebar_get_selection(topic:list)->str:
     return st.sidebar.radio('my category',topic,key="para", on_change=update_params, )
@@ -125,15 +138,11 @@ def show_sidebar_get_selection(topic:list)->str:
 def main():
     st.set_page_config(page_title="Dan's record blog", page_icon=":smile:", layout="wide")
     st.sidebar.title('Navigation')
-    
     exist_topic = ['About me','Read books','my articles','Online course','house trend','subscribe']
     selector = show_sidebar_get_selection(exist_topic)
-    
     if is_empty_option():
         set_option_to_default(exist_topic)
-    
     decide_action_on_selection(selector)
     
 if __name__ == '__main__':
     main()
-
--- suscribe.py
+++ suscribe.py
@@ -1,50 +1,45 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
-def get_big_man_rss():
+def big_man_cutter(data='', cutter='')->str:
+    """
+    A part xml get from bs4 and cutter for certain string to cut 
+    """
+    if (len(data)==0) or (len(cutter)==0):
+        raise ValueError('No Valid data input')
+    data = str(data).split(f'<{cutter}>')[1]
+    data = str(data).split(f'</{cutter}>')[0]
+    return data
+
+def get_big_man_rss()->dict:
     """
     You will get a dict with key is title and value is url
     """
-    def cutter(data='', cutter=''):
-        """
-        A part xml get from bs4 and cutter for certain string to cut 
-        """
-        if (len(data)==0) or (len(cutter)==0):
-            raise ValueError('No Valid data input')
-        data = str(data).split(f'<{cutter}>')[1]
-        data = str(data).split(f'</{cutter}>')[0]
-        return data
-    # check 大人學 RSS
     url = 'https://feeds.feedburner.com/darencademy/GwRT'
-
-    request_site = Request(url, headers = {'User-Agent': 'Mozilla/5.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-            'Accept-Encoding': 'none',
-            'Accept-Language': 'en-US,en;q=0.8',
-            'Connection': 'keep-alive'})
-    webpage = urlopen(request_site).read()
-    # decode get xml
-    soup = BeautifulSoup(webpage )
-    
-    title_raw = soup.select('title')[2:] #first two is header not article mark
-    link_raw = soup.select('guid')
+    raw_html = get_web_html_by_bs4(url)
+    title_raw = raw_html.select('title')[2:] #first two is header not article mark
+    link_raw = raw_html.select('guid')
     if len(title_raw)!=len(link_raw):
             raise ValueError('Title and link len mismatch.')
-    title = [cutter(x, 'title') for x in title_raw]
-    link = [cutter(x, 'guid') for x in link_raw]
+    title = [big_man_cutter(x, 'title') for x in title_raw]
+    link = [big_man_cutter(x, 'guid') for x in link_raw]
     book_dic = dict(zip(title, link))
     return book_dic
 
-def get_green_data():
-    def cutter_list(datas:list):
-        dic = {}
-        for data in datas:
-                dic[str(data).split('a href="')[1].split('">')[1].split('</a>')[0]]=str(data).split('a href="')[1].split('">')[0]
-        return dic
+def green_cutter_list(datas:list)->dict:
+    dic = {}
+    for data in datas:
+        dic[str(data).split('a href="')[1].split('">')[1].split('</a>')[0]]=str(data).split('a href="')[1].split('">')[0]
+    return dic
+
+def get_green_data()->dict:
     #get green first page url
     url = 'https://greenhornfinancefootnote.blogspot.com/'
+    raw_html = get_web_html_by_bs4(url)
+    raw = raw_html.find_all('h3', class_='post-title entry-title')
+    return green_cutter_list(raw)
 
+def get_web_html_by_bs4(url:str):
     request_site = Request(url, headers = {'User-Agent': 'Mozilla/5.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -52,9 +47,5 @@ def get_green_data():
             'Accept-Language': 'en-US,en;q=0.8',
             'Connection': 'keep-alive'})
     webpage = urlopen(request_site).read()
-    # decode get xml
-    soup = BeautifulSoup(webpage )
-    #get new page title
-    raw = soup.find_all('h3', class_='post-title entry-title')
-    # reutnr a dict
-    return cutter_list(raw)
+    soup = BeautifulSoup(webpage)
+    return soup
