3732b5c40fc18ca698aceb1e18d50d3938ca0687
1daniel3333 <p123456dan@gmail.com>
2024-10-28 20:12:13 +0800

    renew suscrib

--- suscribe.py
+++ suscribe.py
@@ -58,13 +58,13 @@ def get_current_day():
 
 def get_comic()->dict:
     comic_dict = {
-        "Monday":{'第一序列':'https://www.colamanga.com/manga-pp95549/','全知读者视角':'https://www.colamanga.com/manga-gs015814/'}, 
-        "Tuesday":{'我独自满级新手':'https://www.colamanga.com/manga-pl703354/'}, 
+        "Monday":{'第一序列':'https://www.colamanga.com/manga-pp95549/','全知读者视角':'https://www.colamanga.com/manga-cw639193/'}, 
+        "Tuesday":{'我独自满级新手':'https://www.colamanga.com/manga-qc474017/'}, 
         "Wednesday":{'全球冰封：我打造了末日安全屋':'https://www.colamanga.com/manga-hy703661/'}, 
-        "Thursday":{'66666年后复活的黑魔法师':'https://www.colamanga.com/manga-yg780527/'}, 
-        "Friday":{'看脸时代':'https://www.colamanga.com/manga-nn727564/'}, 
-        "Saturday":{'这个勇者是金钱至上主义者':'https://www.colamanga.com/manga-rp47086/','觉醒战士':'https://www.colamanga.com/manga-gg528682/','死灵法师！我即是天灾':'https://www.colamanga.com/manga-eg195836/'}, 
-        "Sunday":{'炼体十万层：我养的狗都是大帝':'https://www.colamanga.com/manga-qq128585/','被塔诅咒的猎人':'https://www.colamanga.com/manga-dg260221/','99强化木棍':'https://www.colamanga.com/manga-kh997348/'},
+        "Thursday":{'66666年后复活的黑魔法师':'https://www.colamanga.com/manga-ai600178/'}, 
+        "Friday":{'看脸时代':'https://www.colamanga.com/manga-hn661197/'}, 
+        "Saturday":{'这个勇者是金钱至上主义者':'https://www.colamanga.com/manga-rp47086/','觉醒战士':'https://www.colamanga.com/manga-es137754/','死灵法师！我即是天灾':'https://www.colamanga.com/manga-eg195836/'}, 
+        "Sunday":{'炼体十万层：我养的狗都是大帝':'https://www.colamanga.com/manga-qq128585/','99强化木棍':'https://www.colamanga.com/manga-kh997348/','任务至上主义':'https://www.colamanga.com/manga-vc722396/',},
     }
     current = get_current_day()
     return comic_dict[current]
