c5587566a79e3133992b2262649769b73166ea52
1daniel3333 <p123456dan@gmail.com>
2024-11-04 20:14:19 +0800

    debug add st

--- topic.py
+++ topic.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 def weather_main():
     body = '''<iframe src="https://www.cwa.gov.tw/V8/C/W/OBS_Radar.html" width="800" height="600" frameborder="0" allowfullscreen></iframe>'''
     st.markdown(body, unsafe_allow_html=True)
