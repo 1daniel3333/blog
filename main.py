addc548806418655e0857a0d772a598f8cb5e0d8
1daniel3333 <p123456dan@gmail.com>
2025-01-10 23:37:38 +0800

    rm llm

--- main.py
+++ main.py
@@ -33,8 +33,6 @@ def get_action_online_course():
 
 def get_action_about_me():
     about.about()
-    import llama32_st as llm
-    llm.llama_talk()
 
 def get_action_house_trend():
     house.house_main()
